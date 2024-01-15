5491c048fb275c9f2946c5428962816d628cb378
sirajlekhak <crajmusics@gmail.com>
2024-01-15 11:41:00 +0530

    Added project section

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,15 +2,17 @@ import styles from "./App.module.css";
 import { Navbar } from "./components/Navbar/Navbar";
 import { Hero } from "./components/Hero/Hero";
 import { About } from "./components/About/About";
-import {Experience} from "./components/Experience/Experience";
+import { Experience } from "./components/Experience/Experience";
+import { Projects } from "./components/Projects/Projects"
 
 function App() {
   return (
     <div className={styles.App}>
       <Navbar />
-      <Hero/>
-      <About/>
-      <Experience/>
+      <Hero />
+      <About />
+      <Experience />
+      <Projects />
     </div>
   );
 }
